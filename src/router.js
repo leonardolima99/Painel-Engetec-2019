a8b89d1c98e7ef9da664afaf3cef7df60dfe8082
Leonardo Lima <leonardo1230987@gmail.com>
2019-07-03 23:22:48 -0400

    Adicionado rota para página 404

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
 import Postagens from './components/Postagens.vue'
+import Error404 from './components/Error404.vue'
 
 Vue.use(Router)
 
@@ -30,9 +31,8 @@ export default new Router({
       component: Home
     },
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/*',
+      component: Error404
     },
     
   ]
